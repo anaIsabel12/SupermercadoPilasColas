42a123bbc06618f368106a0fc1d5747115a26720
anaIsabel12 <103321446+anaIsabel12@users.noreply.github.com>
2022-04-28 11:29:16 +0100

    Feature: Añadida clase de utilidades aleatorias

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import com.digitalCastaway.utils.SupermarketUtils;
+
 public class Main {
 
     public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println((SupermarketUtils.generarNombreAleatorio()));
+            System.out.println((SupermarketUtils.generarNombreProductoAleatorio()));
 
+        }
 
     }
 }
--- src/com/digitalCastaway/utils/SupermarketUtils.java
+++ src/com/digitalCastaway/utils/SupermarketUtils.java
@@ -1,5 +1,40 @@
 package com.digitalCastaway.utils;
 
 public class SupermarketUtils {
+    private static final String []NOMBRES_PERSONA = {
+            "Goyo",
+            "Luecrecia",
+            "Montese",
+            "Francisco",
+            "Nicolás",
+            "Yolanda",
+            "Matias",
+            "Lionel",
+            "Igor",
+    };
+    public static final String []NOMBRES_PRODUCTOS = {
+            "Pistacho",
+            "Millo",
+            "Pasas",
+            "Leche",
+            "Tortilla",
+            "Pasticho",
+            "Gofio",
+            "Plátano",
+            "Pan de pipas",
+            "Merengue",
+            "Tacos salmón",
+            "Takis",
+            "Chetos Pelotazo",
+    };
 
+    /* generar método para hacer estas cosas numero aleatorio de personas entre cero y el tamaño del array length*/
+
+    public static String generarNombreAleatorio() {
+        return NOMBRES_PERSONA[(int) (Math.random () * NOMBRES_PERSONA.length)];
+    }
+
+    public static String generarNombreProductoAleatorio() {
+        return NOMBRES_PRODUCTOS[(int) (Math.random () * NOMBRES_PRODUCTOS.length)];
+    }
 }
